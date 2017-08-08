8b439823cf9d6fbd531ef2e49f4daa8711727903
jy19921020 <691096060@qq.com>
2017-08-08 13:49:34 +0800

    8.8.3修改

--- springboot-jpa/build.sh
+++ springboot-jpa/build.sh
@@ -2,7 +2,7 @@
 
 source /etc/profile
 
-MYIMAGE=192.168.1.2:8082/springboot/springboot-jpa
+MYIMAGE=127.0.0.1:8008/test/springboot-jpa
 
 # uncomment if you need push
 #docker login 192.168.1.2:8082 -u admin -p admin123
@@ -14,13 +14,13 @@ docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
 
 # remove old images
-docker images | grep 192.168.1.2:8082/springboot/springboot-jpa | awk '{print $3}' | xargs docker rmi
+docker images | grep 127.0.0.1:8008/test/springboot-jpa | awk '{print $3}' | xargs docker rmi
 
 # build jar and image
 mvn package -e -X docker:build -DskipTest
 
 # running container
-docker run -dp 8080:8080 --name springboot-jpa ${MYIMAGE}
+docker run -dp 8888:8008 --name springboot-jpa ${MYIMAGE}
 
 # push image
 #docker push ${MYIMAGE}
